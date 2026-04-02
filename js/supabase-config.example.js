0d0511ebcf76efcdc23198e2e61262f7e68ccf52
Ahmet Ataş <ahmetatas23@gmail.com>
2026-04-02 11:21:13 +0300

    Update supabase-config.example.js

--- js/supabase-config.example.js
+++ js/supabase-config.example.js
@@ -5,8 +5,8 @@
  * Ardından gerçek değerleri supabase-config.js içine yazın.
  */
 
-const SUPABASE_URL      = https://gmzdeptaipfacmfdexnd.supabase.co;
-const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtemRlcHRhaXBmYWNtZmRleG5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3Mzk2NzAsImV4cCI6MjA3MjMxNTY3MH0.uFenHsXWA0esrAoAYJ3MK6UoEW-8uNOc8CzYrJVyt5k;      // Örn: eyJhbGci...
+const SUPABASE_URL      = 'https://gmzdeptaipfacmfdexnd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtemRlcHRhaXBmYWNtZmRleG5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3Mzk2NzAsImV4cCI6MjA3MjMxNTY3MH0.uFenHsXWA0esrAoAYJ3MK6UoEW-8uNOc8CzYrJVyt5k';      // Örn: eyJhbGci...
 ';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
